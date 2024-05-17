30fe74c53a3862ebef7fdad915544a540fbd358d
rikiMaman <148782616+rikiMaman@users.noreply.github.com>
2024-05-17 03:09:28 +0300

    user 2 changes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import JobsRouter from './JobsRouter.js';
 import index from './index.html';
 
 const app= express()
-const arr= [1,2,3,4,5];
+const arrey= [1,2,3,4,5];
+console.log(arrey[2]);
+
 app.use(cors())
 app.use(bodyParser.json())
 app.get('/', (req, res)=>{
@@ -16,3 +18,4 @@ app.use('/jobs', JobsRouter)
 app.listen(5001, () => {
     console.log('app is running on http://localhost:5001')
 })
+console.log('user 2 conflict');
